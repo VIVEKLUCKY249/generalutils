9d5bf87b6f46e3a91c9972e1a47087509f0011f2
VS <viveks@elayers.net>
2015-10-11 23:10:27 +0530

    Update magento_module_dirtree_maker.bat

--- magento_module_dirtree_maker.bat
+++ magento_module_dirtree_maker.bat
@@ -1,7 +1,9 @@
 REM Script to create multiple directory structure for Magento Modules
 @echo off
-setlocal EnableDelayedExpansion
+SETLOCAL EnableDelayedExpansion
+SETLOCAL EnableExtensions
 
+SET "TAB=	"
 SET moduleApp="Magento Module\app"
 SET moduleSkin="Magento Module\skin"
 SET moduleJs="Magento Module\js"
@@ -11,3 +13,18 @@ md %moduleApp%/code/local
 md %moduleApp%/etc/modules
 md %moduleApp%/design/adminhtml/default/default/layout
 md %moduleApp%/design/frontend/base/default/layout
+
+cd %moduleApp%/etc/modules/
+(
+echo ^<?xml version="1.0" encoding="UTF-8"?^>
+echo ^<config^>
+	echo %tab%^<modules^>
+		echo %tab%%tab%^<Vivek_Multigrid^>
+			echo %tab%%tab%%tab%^<active^>true^</active^>
+			echo %tab%%tab%%tab%^<codePool^>local^</codePool^>
+		echo %tab%%tab%^</Vivek_Multigrid^>
+	echo %tab%^</modules^>
+echo ^</config^>
+) > "module.xml"
+
+pause
