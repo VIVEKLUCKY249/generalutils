11598ec7d5a0d36ae0a5663f768b3b00dc1c5c47
VS <VIVEKLUCKY1848@users.noreply.github.com>
2016-05-16 08:01:07 +0530

    Create removeVirus.cmd

---
+++ removeVirus.cmd
@@ -0,0 +1,20 @@
+sc stop Airtostrong
+sc delete Airtostrong
+PAUSE
+
+sc stop AppgnortsotriA
+sc delete AppgnortsotriA
+PAUSE
+
+sc stop sulpnar
+sc delete sulpnar
+PAUSE
+
+sc stop xifs
+sc delete xifs
+PAUSE
+
+goto comment
+Create a shortcut to this file and set it to "Run as Administrator" and 
+after running this check if there is any executable with junk character in name still in "C:\Program Files\Common Files"
+:comment
